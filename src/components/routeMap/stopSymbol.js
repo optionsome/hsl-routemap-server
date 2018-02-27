9ec2efc90b95320302afdf9135f61a38992dbe48
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-02-27 13:48:47 +0200

    Color stops a bit darker than routes

--- src/components/routeMap/stopSymbol.js
+++ src/components/routeMap/stopSymbol.js
@@ -1,15 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { colorsByMode } from "util/domain";
-
 const StopSymbol = props => (
     <svg width={props.size} height={props.size} style={{ display: "block" }}>
         <circle
             cx={props.size / 2}
             cy={props.size / 2}
             r={props.size / 2}
-            fill={colorsByMode.BUS}
+            fill="#00558c"
         />
     </svg>
 );
