7280f3dcaca899ffe3ff9b903bdbdfb0e1275ce9
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-04-05 16:08:57 +0300

    Add type to database schema

---
+++ migrations/20180405155100_add_routemap_types.js
@@ -0,0 +1,13 @@
+exports.up = async (knex) => {
+    const posterType = ["POSTER", "ROUTEMAP"];
+
+    await knex.schema.table("build", (table) => {
+        table.enum("type", posterType).defaultTo("POSTER").notNullable();
+    });
+};
+
+exports.down = async (knex) => {
+    await knex.schema.table("build", (table) => {
+        table.dropColumn("type");
+    });
+};
