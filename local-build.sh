b5bc62473c39f580f2e390d7a66ed4bbd2dab5f2
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-07-03 09:12:20 +0300

    Fix build scripts.

--- local-build.sh
+++ local-build.sh
@@ -6,5 +6,5 @@ DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest}
 DOCKER_IMAGE=$ORG/hsl-routemap-server:${DOCKER_TAG}
 DOCKER_IMAGE_LATEST=$ORG/hsl-routemap-server:latest
 
-docker build --tag=$DOCKER_IMAGE_LATEST .
+docker build --build-arg BUILD_ENV=production --tag=$DOCKER_IMAGE_LATEST .
 docker push $DOCKER_IMAGE_LATEST
