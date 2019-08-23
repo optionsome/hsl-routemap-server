6e66b5b32b1452a48d472e03a6ce8079a412d5f1
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-08-23 09:59:21 +0300

    Read Azure secrets from docker secrets, some other improvements.

--- knexfile_jore.js
+++ knexfile_jore.js
@@ -1,8 +1,10 @@
-if (!process.env.PG_JORE_CONNECTION_STRING) {
+const { PG_JORE_CONNECTION_STRING } = require('./constants');
+
+if (!PG_JORE_CONNECTION_STRING) {
   throw new Error('PG_JORE_CONNECTION_STRING variable is not set');
 }
 
 module.exports = {
   client: 'pg',
-  connection: process.env.PG_JORE_CONNECTION_STRING,
+  connection: PG_JORE_CONNECTION_STRING,
 };
