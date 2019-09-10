7c953a99eb7a068f24559ede38b540fa3803ef27
Juuso Korhonen <juseek@gmail.com>
2019-09-10 13:23:37 +0300

    refactor(jorestore) add func for querying current point date

--- scripts/joreStore.js
+++ scripts/joreStore.js
@@ -12,6 +12,10 @@ async function generatePoints(date) {
   return knex.raw('select * from jorestatic.create_intermediate_points(?)', [date]);
 }
 
+async function getPointGenerationDate() {
+  return knex.raw('select tag from jorestatic.intermediate_points order by tag desc limit 1');
+}
 module.exports = {
   generatePoints,
+  getPointGenerationDate
 };
